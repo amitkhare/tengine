771af9ad0e363e7409dd1c62d2d6f938c3e75375
Amit Kumar Khare <amit@khare.co.in>
2016-05-11 21:48:05 +0530

    update

--- AmitKhare/Tengine/tengine.php
+++ AmitKhare/Tengine/tengine.php
@@ -2,7 +2,11 @@
 namespace AmitKhare\Tengine;
 
 class Tengine {
-
+	private $modulesPath;
+	private $templatesPath;
+	private $template;
+	private $;
+	private $;
 	function __construct(){
 		die('It does noting yet, but it will soon.');
 	}
